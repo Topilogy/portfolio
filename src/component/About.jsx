e948bcc7dcf917497ff4e5a80c4c812703dde226
Topilogy <36141664+Topilogy@users.noreply.github.com>
2023-11-22 18:57:11 +0100

    code

---
+++ src/component/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" 
+         className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col  justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    About
+                </p>
+
+                <p className='text-1xl mt-20'>
+                    I pursued my career in computer science from 2018 to 2022, developing a strong foundation for logical thinking. My passion for 
+                    coding  led me to venture into web development in 2021. My journey began with a fascination for HTML, CSS and Javascript and its. 
+                    The joy of working with function-based components in Reactjs, followed by the satisfying "npm start" command in the terminal, 
+                    quickly became a favorite ritual. However, my heart lies in web development. I've actively maintained and updated my Git repositories, 
+                    ensuring they reflect my growth and commitment to the field.
+                </p> 
+
+                <br /> 
+                <p className='text-1xl'>
+                    I am actively seeking an entry-level position as a Frontend Developer, leveraging my expertise in ReactJs. With over a year of valuable insights 
+                    gained during my internship training in zuri as a Frontend developer, I bring a deep understanding of workspace culture. My commitment to excellence 
+                    is reflected in the 5+ projects I've undertaken, ranging from small to large scale. These projects, showcased on my GitHub repositories, demonstrate 
+                    my proficiency in pure JavaScript and ReactJS. I've employed various technologies such as JSON server, Render.com, React Routers, React-Tables, 
+                    Tailwind css and Chrome extensions. This diverse skill set allows me to adapt and contribute effectively to different project requirements. 
+                    I am always ready to go the extra mile, both in code and communication, to simplify processes. Currently, I am expanding my knowledge base by delving 
+                    into Nodejs, TypeScript and NextJs through practical projects. Feel free to explore my portfolio for a detailed overview of my work.
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/component/Portfolio.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import Recipes from "../assets/food-recipe.png"
+import Hangman from "../assets/hangman-game.png"
+import Todo from "../assets/to-do-list.png"
+import Port from "../assets/portfolio.png"
+
+const Portfolio = () => {
+
+    const portfolios = [
+        {
+            id: 1,
+            src: Recipes, 
+            txt: "Recipes",
+            chref: "https://github.com/Topilogy/recipe-app",
+            dhref: "https://recipe-app-one-ashen.vercel.app/",
+        },
+        {
+            id: 2,
+            src: Hangman,
+            txt: "Hangman Game",
+            chref: "https://github.com/Topilogy/hangman",
+            dhref: "https://hangman-teal-eight.vercel.app/",
+        },
+        {
+            id: 3,
+            src: Todo,
+            txt: "To-do-List",
+            chref: "https://github.com/Topilogy/To-do-list",
+            dhref: "https://to-do-list-ochre-three-74.vercel.app/",
+        },
+        {
+            id:4,
+            src: Port,
+            txt: "Portfolio",
+            chref: "https://github.com/Topilogy/topilogy-portfolio",
+            dhref: "https://topilogy-portfolio-beta-two.vercel.app/",
+        },
+    ]
+
+  return (
+    <div 
+        name="portfolio" 
+        className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    Portfolio
+                </p>
+                <p className='py-6'>Check out some of my work right here</p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+            
+                {portfolios.map(({id, src, txt, dhref, chref}) =>(
+                    <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                        <img 
+                            src={src} 
+                            alt='' 
+                            className='rounded-md duration-200 hover:scale-105' 
+                        />
+                        <div className='absolute top-16 pr-2 text-3xl duration-200 text-white  hidden group-hover:block'>{txt}</div>
+                        <div className='flex items-center justify-center'>
+                            <div>
+                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 cursor-pointer'>
+                                <a 
+                                    href={dhref} 
+                                     
+                                    target="_blank" 
+                                    rel="noreferrer" 
+                                >
+                                    Demo
+                                </a>
+                                </button>
+                            </div>
+
+                            <div>
+                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 cursor-pointer'>
+                                <a
+                                    href={chref} 
+                                    
+                                    target="_blank" 
+                                    rel="noreferrer"
+                                >
+                                    Code
+                                </a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+       
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio;
